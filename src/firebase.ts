4522a85556d909fdf5bc54e405b8171fa6b01a2a
Shaina Talisay <talisayshaina8@gmail.com>
2025-05-19 14:24:04 +0800

    added env

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDK-3nSouPkt9nxvCJuxbxY8mlN6Q3rJpQ",
-  authDomain: "react-to-do-list-c5789.firebaseapp.com",
-  projectId: "react-to-do-list-c5789",
-  storageBucket: "react-to-do-list-c5789.firebasestorage.app",
-  messagingSenderId: "10990347249",
-  appId: "1:10990347249:web:15af8b51d49c86f37d1a50",
-  measurementId: "G-VWNT682MW2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
